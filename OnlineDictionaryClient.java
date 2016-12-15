007311cc776ab78ef9e5777121556455a1073358
akwing <wqant@foxmail.com>
2016-12-15 23:06:03 +0800

    添加了注册登录界面

--- OnlineDictionaryClient.java
+++ OnlineDictionaryClient.java
@@ -58,10 +58,10 @@ public class OnlineDictionaryClient extends JFrame implements ActionListener
     	super("在线词典ver1.0");
     	hasLogin = false;
     	setResizable(false);
-    	this.setSize(800, 600);
+    	this.setSize(700, 600);
     	Toolkit tk = this.getToolkit();
     	Dimension dm = tk.getScreenSize(); 
-    	this.setLocation((int)(dm.getWidth()-800)/2,(int)(dm.getHeight()-600)/2);
+    	this.setLocation((int)(dm.getWidth()-700)/2,(int)(dm.getHeight()-700)/2);
 
     	//菜单栏
     	logMenu.add(jmi1);
@@ -162,6 +162,8 @@ public class OnlineDictionaryClient extends JFrame implements ActionListener
 				}
 			}
 		});
+    	
+    	//用户注册按钮
     	jmi1.addActionListener(new ActionListener(){
     		public void actionPerformed(ActionEvent e)
     		{
@@ -186,6 +188,30 @@ public class OnlineDictionaryClient extends JFrame implements ActionListener
     		}
     	});
     	
+    	//用户登陆按钮
+    	jmi2.addActionListener(new ActionListener(){
+    		public void actionPerformed(ActionEvent e)
+    		{
+    			String username, password;
+    			//username = JOptionPane.showInputDialog("请输入用户名");
+    			//password = JOptionPane.showInputDialog("请输入密码");
+    			
+    			//Message m = new Message(Message.SIGNIN, username, password);
+    			
+    			ObjectOutputStream toServer;
+    			ObjectInputStream fromServer;
+    			Runnable task = new LoginTask();
+    			new Thread(task).start();
+    			/*try {
+					Socket socket = new Socket("172.26.91.180",8006);
+					toServer =  new ObjectOutputStream(socket.getOutputStream());
+					fromServer = new ObjectInputStream(socket.getInputStream());
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}*/
+    		}
+    	});
     	this.setVisible(true);
     }
     
@@ -255,6 +281,85 @@ public class OnlineDictionaryClient extends JFrame implements ActionListener
 			add(jlab1);add(jtf);
 			add(jlab2);add(jpw);
 			add(jbt1);add(jbt2);
+			
+			Toolkit tk = this.getToolkit();
+	    	Dimension dm = tk.getScreenSize(); 
+	    	this.setLocation((int)(dm.getWidth()-300)/2,(int)(dm.getHeight()-200)/2);
+	    	
+	    	jbt1.addActionListener(new ActionListener(){
+	    		public void actionPerformed(ActionEvent e)
+	    		{
+	    			dispose();
+	    		}
+	    	});
+	    	jbt2.addActionListener(new ActionListener(){
+	    		public void actionPerformed(ActionEvent e)
+	    		{
+	    			dispose();
+	    		}
+	    	});
+		}
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			this.setVisible(true);
+		}
+	}
+	
+	class LoginTask extends JFrame implements Runnable{
+		
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		JLabel jlab1 = null;
+		JLabel jlab2 = null;
+		JButton jbt1 = null;
+		JButton jbt2 = null;
+		JTextField jtf = null;
+		JPasswordField jpw = null;
+		
+		public LoginTask()
+		{
+			super("用户登陆");
+			this.setResizable(false);
+			jlab1 = new JLabel("账号");
+			jlab2 = new JLabel("密码");
+			jbt1 = new JButton("确定");
+			jbt2 = new JButton("取消");
+			jtf = new JTextField();
+			jpw = new JPasswordField();
+			
+			setLayout(null);
+			
+			jlab1.setBounds(60,50,40,25);
+			jlab2.setBounds(60,90,40,25);
+			jbt1.setBounds(80,120,70,32);
+			jbt2.setBounds(160,120,70,32);
+			jtf.setBounds(90,50,120,25);
+			jpw.setBounds(90,90,120,25);
+			
+			setBounds(500,400,300,200);
+			add(jlab1);add(jtf);
+			add(jlab2);add(jpw);
+			add(jbt1);add(jbt2);
+			
+			Toolkit tk = this.getToolkit();
+	    	Dimension dm = tk.getScreenSize(); 
+	    	this.setLocation((int)(dm.getWidth()-300)/2,(int)(dm.getHeight()-200)/2);
+	    	
+	    	jbt1.addActionListener(new ActionListener(){
+	    		public void actionPerformed(ActionEvent e)
+	    		{
+	    			dispose();
+	    		}
+	    	});
+	    	jbt2.addActionListener(new ActionListener(){
+	    		public void actionPerformed(ActionEvent e)
+	    		{
+	    			dispose();
+	    		}
+	    	});
 		}
 		@Override
 		public void run() {
